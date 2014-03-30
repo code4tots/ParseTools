009d818abb9d25e8c54af5ddc8f4ab4ed056152b
math4tots <math4tots@gmail.com>
2014-03-29 17:41:17 -0700

    vv

--- parse.py
+++ parse.py
@@ -1,181 +1,94 @@
-class ParseException(Exception):
-    'base exception'
-    def __init__(self,stream=None):
-        self.set_stream(stream)
-    
-    def set_stream(self,stream):
-        self.stream = stream
-        self.index = stream.index
-        self.callstack = list(stream.callstack)
-
-class ParseFatal(ParseException):
-    'unrecoverable -- should be caught by user'
-
-class LeftRecursion(ParseFatal):
-    'left recursion indicates something wrong with grammar'
-
-class ParseFailed(ParseException):
-    'thrown around a lot when pattern does not match'
-
-class ActionFailed(ParseFailed):
-    'when user action indicates parse should fail'
-
 class Stream(object):
-    'Should be subclassed for extra behavior (e.g. indent stack)'
     def __init__(self,string):
         self.string = string
         self.index = 0
-        self.callstack = []
-        self.callers = set()
-        self.memo_value = dict()
-        self.memo_index = dict()
+        self.memo = dict()
+    
+    @property
+    def state(self):
+        return self.index
+    
+    @state.setter
+    def state(self,new_state):
+        self.index = new_state
 
-fail_indicator = object()
+class ParseFailed(Exception):
+    pass
 
-class Parser(object):
-    'subclasses should override __init__ and _parse'
+parse_failed = object()
+class BaseParser(object):
+    def parse(self,stream):
+        if isinstance(stream,str):
+            stream = Stream(stream)
+        
+        old_state = stream.state
+        
+        if (self,old_state) not in stream.memo:
+            try:
+                return_value = self._parse(stream)
+                new_state = stream.state
+                stream.memo[self,old_state] = return_value, new_state
+        
+            except ParseFailed:
+                stream.state = old_state
+                stream.memo[self,old_state] = None, parse_failed
+        return_value, new_state = stream.memo[self,old_state]
+        
+        if new_state is parse_failed:
+            raise ParseFailed(stream)
+        
+        stream.state = new_state
+        return return_value
+    
+    def __and__(self,other):
+        return AndParser(self,other)
+    
+    def __or__(self,other):
+        return OrParser(self,other)
+
+class Parser(BaseParser):
     def __init__(self,parser=None):
         self.parser = parser
     
     def _parse(self,stream):
-        return self.parser(stream)
-    
-    def __call__(self,stream):
-        if isinstance(stream,str):
-            stream = Stream(stream)
-        
-        h = id(self) * len(stream.string) + stream.index
-        
-        if h in stream.callers:
-            raise LeftRecursion(stream)
-        
-        if h not in stream.memo_value:
-            stream.callstack.append((self,stream.index))
-            stream.callers.add(h)
-            
-            try:
-                stream.memo_value[h] = self._parse(stream)
-                stream.memo_index[h] = stream.index
-                
-            except ParseFailed:
-                stream.memo_value[h] = fail_indicator
-                raise
-                
-            finally:
-                stream.callstack.pop()
-                stream.callers.remove(h)
-        
-        if stream.memo_value[h] is fail_indicator:
-            raise ParseFailed(stream)
-        
-        stream.index = stream.memo_index[h]
-        return stream.memo_value[h]
-    
-    def __and__(self,other):
-        a = self .parsers if isinstance(self ,And) else [self ]
-        b = other.parsers if isinstance(other,And) else [other]
-        return And(a+b)
-    
-    def __or__(self,other):
-        a = self .parsers if isinstance(self ,Or) else [self ]
-        b = other.parsers if isinstance(other,Or) else [other]
-        return Or(a+b)
-    
-    def __add__(self,other):
-        return Second(self,other)
-    
-    def __sub__(self,other):
-        return First(self,other)
-    
-    def __lt__(self,action):
-        return Action(self,action)
-    
-    def __lshift__(self,alternatives):
-        return Reduce(self,alternatives)
+        return self.parser.parse(stream)
 
-class Regex(Parser):
-    'essentially all terminals'
+class RegexParser(BaseParser):
     def __init__(self,regex):
         import re
         self.regex = re.compile(regex)
     
     def _parse(self,stream):
-        match = self.regex.match(stream.string,stream.index)
-        if match is None:
+        m = self.regex.match(stream.string,stream.index)
+        if m is None:
             raise ParseFailed(stream)
-        stream.index = match.end()
-        return match.group()
+        stream.index = m.end()
+        return m.group()
 
-class And(Parser):
-    'Fixed number -- if count unknown, should prefer a chaining mechanism'
-    def __init__(self,parsers):
-        self.parsers = parsers
-    
-    def _parse(self,stream):
-        return [parser(stream) for parser in self.parsers]
-    
-    def __lt__(self,action):
-        return Action(self,lambda xs : action(*xs))
-
-class Or(Parser):
-    'one of alternatives'
-    def __init__(self,parsers):
-        self.parsers = parsers
-    
-    def _parse(self,stream):
-        index = stream.index
-        for parser in self.parsers:
-            try:                return parser(stream)
-            except ParseFailed: stream.index = index
-        raise ParseFailed(stream)
-
-class First(Parser):
-    'keeps just the result of first parser'
+class AndParser(BaseParser):
     def __init__(self,a,b):
         self.a = a
         self.b = b
     
     def _parse(self,stream):
-        x = self.a(stream)
-        self.b(stream)
-        return x
+        return (self.a.parse(stream),self.b.parse(stream))
 
-class Second(Parser):
-    'keeps just the result of the second parser'
+class OrParser(BaseParser):
     def __init__(self,a,b):
         self.a = a
         self.b = b
     
     def _parse(self,stream):
-        self.a(stream)
-        return self.b(stream)
+        try:
+            return self.a.parse(stream)
+        except ParseFailed:
+            pass
+        
+        return self.b.parse(stream)
 
-class Action(Parser):
-    'changes return value'
-    def __init__(self,parser,action):
-        self.parser = parser
-        self.action = action
-    
+class EmptyParser(BaseParser):
+    'singleton'
     def _parse(self,stream):
-        x = self.parser(stream)
-        return self.action(x)
-
-class Reduce(Parser):
-    'to aid in left recursive grammars'
-    def __init__(self,parser,alternatives):
-        self.parser = parser
-        self.alternatives = alternatives
+        pass
     
-    def _parse(self,stream):
-        x = self.parser(stream)
-        while True:
-            match_begin = stream.index
-            
-            for parser, action in self.alternatives:
-                try:                x = action(x,parser(stream))
-                except ParseFailed: stream.index = match_begin
-                else:               break
-                
-            if match_begin == stream.index:
-                break
-        return x
+empty_parser = EmptyParser()
---
+++ test.py
@@ -0,0 +1,48 @@
+from parse import Parser, RegexParser as RP, empty_parser as empty
+
+space = RP(r'\s*')
+token = lambda r : space & RP(r)
+equal = token(r'\=')
+name  = token(r'\w+')
+slit  = token(r'\"((\\\")|[^\"])*\"')
+vline = token(r'\|')
+aand  = token(r'\&')
+opar  = token(r'\(')
+cpar  = token(r'\)')
+obra  = token(r'\[')
+cbra  = token(r'\]')
+end   = token(r'$')
+
+expr         = Parser()
+
+exprs        = Parser()
+exprs.parser = expr & exprs | empty
+
+pexpr        = slit | name | (opar & expr & cpar & empty)
+
+fex          = Parser()
+fex.parser   = obra & exprs & cbra & empty & fex | empty
+fexpr        = pexpr & fex | pexpr
+
+aex          = Parser()
+aex.parser   = aand & fexpr & aex | empty
+aexpr        = fexpr & aex | fexpr
+
+vex          = Parser()
+vex.parser   = vline & aexpr & vex | empty
+vexpr        = aexpr & vex | aexpr
+
+expr.parser  = vexpr
+
+line         = name & equal & expr
+
+lines        = Parser()
+lines.parser = line & lines | end
+
+p = lines.parse('''
+f = x:f[a b]
+''')
+print(p)
+
+
+
